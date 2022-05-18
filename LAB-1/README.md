108dfdf852aaab060780043d687170d80beae656
Sreemanto <37799501+Sreemanto@users.noreply.github.com>
2022-05-17 17:58:30 -0700

    Add files via upload

---
+++ LAB-1/README.md
@@ -0,0 +1,4 @@
+# test
+CPSC 4800 git test
+Sreemanta Kesh 
+100373588
---
+++ LAB-1/src/hello_world.py
@@ -0,0 +1,8 @@
+'''
+First Program with Git
+'''
+print("hello world")
+# A comment line added
+print("Welcome to Data Analytics")i
+name = input("What is your name ?")
+print("Welcome {name} to Data Analytics Course")
